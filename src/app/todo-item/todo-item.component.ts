2998a6838a3d5b03b8bdf0c3c6aa3c57579deac3
kannysa14 <kgml94@hotmail.com>
2021-12-03 14:54:27 +0100

    Interface

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
 
 @Component({
   selector: 'app-todo-item',
   template: `
-    {{ item.title }}
+    {{ item?.title }}
   `,
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent implements OnInit {
 
-  @Input() item: any;
+  @Input() item: TodoItem | undefined;
   constructor() { }
 
   ngOnInit(): void {
